307b30b71ec0995f0b03851926c054bdf99b09fe
Declan W <148910875+DeclanW1@users.noreply.github.com>
2025-06-28 12:42:02 +0100

    Made changes to header component - Added logo and changed spacing

--- src/app/homepage/header.tsx
+++ src/app/homepage/header.tsx
@@ -1,10 +1,20 @@
-export default function Header() {
-    return (
-      <div className="text-center mt-8">
-        <h1 className="text-4xl md:text-5xl font-serif font-bold text-[#3b2f2f] drop-shadow-md">
-          Welcome to Brightwell Academy
-        </h1>
-      </div>
-    )
-  }
-  
+export default function Header(){
+  return (
+    <header className="relative w-full h-20 mt-8">
+      <img src="/logo.png" alt="Brightwell Academy Logo" className="absolute left-8 top-[55%] transform -translate-y-1/2 w-32 h-auto"/>
+
+      <h1 className="m-0 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl md:text-5xl font-serif font-bold text-[#3b2f2f] drop-shadow-md whitespace-nowrap">
+        Welcome to Brightwell Academy
+      </h1>
+    </header>
+  )
+}
+
+
+
+
+
+
+
+
+
