13bfd3f3a3f2fab4b6bf06b6049a047faac49753
Declan W <148910875+DeclanW1@users.noreply.github.com>
2025-07-08 11:29:23 +0100

    Added Website Head

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -3,10 +3,15 @@ import Boxes from './boxes'
 
 export default function HomePage() {
     return (
+      <>
+      <Head>
+        <title>Brightwell Academy</title>
+      </Head>
       <main className="min-h-screen overflow-hidden">
         <Header />
         <Boxes />
       </main>
+      </>
     )
   }
   
