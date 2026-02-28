9803fc89d99dc4e7b17080eee5168df39f90144c
Paul Aldred-Bann <paul@bigthinkingltd.com>
2026-02-28 17:31:43 +0000

    updated form styling and added text

--- src/app/reckoning/page.tsx
+++ src/app/reckoning/page.tsx
@@ -55,7 +55,7 @@ const TheForm = () => {
   };
 
   return (
-    <form className="max-w-2xl min-w-md" onSubmit={handleSubmit}>
+    <form className="max-w-2xl" onSubmit={handleSubmit}>
       <div className="space-y-5">
         <div>
           <label htmlFor="name" className="md:text-md mb-2 block text-sm text-red-600">
@@ -112,13 +112,13 @@ const TheForm = () => {
           <span>I consent to receiving emails from Brightwell</span>
         </label>
 
-        <p className="text-xs text-red-700 italic">
+        <p className="text-xs text-black/90 italic">
           We will use your name and home address solely for the purpose of sending your Brightwell
           letter. Your postal details will not be added to any mailing list and will not be used
           again after your letter has been sent.
         </p>
 
-        <p className="text-xs text-red-700 italic">
+        <p className="text-xs text-black/90 italic">
           Your email address will only be used to contact you about The Reckoning and, if you choose
           to opt in, to receive updates from Brightwell and Darkwell Academies. You can choose
           whether or not to join the Brightwell mailing list using the option below. You may
@@ -150,12 +150,19 @@ const TheForm = () => {
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen overflow-hidden bg-white">
+    <main className="flex min-h-screen items-center justify-end overflow-hidden bg-white">
       <div
         aria-hidden="true"
         className="pointer-events-none absolute inset-0 bg-[url(/raven.svg)] bg-size-[auto_700px] bg-bottom-left bg-no-repeat opacity-40 lg:bg-size-[auto_900px] 2xl:bg-size-[auto_1200px]"
       />
-      <div className="flex min-h-screen items-center justify-end px-10">
+      <div className="flex flex-col gap-y-2 px-2 sm:px-8">
+        <h3 className="text-xl font-bold text-red-600">You found the Raven!</h3>
+        <p className="text-start text-xs">Not everyone does.</p>
+        <p className="tracking-light text-xs">Those who notice will not be overlooked.</p>
+        <p className="tracking-light mb-6 text-xs">
+          If you wish to receive official correspondence from Darkwell Academy, leave your details
+          below. A sealed letter will be sent to you.
+        </p>
         <TheForm />
       </div>
     </main>
