51d92ec02de2a84f2a77f9799b60585dda741945
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-03-17 11:09:37 +0000

    Added two homepage buttons, 'Find My House' and 'History', changed their colours

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,6 +82,20 @@ export default function Home() {
           >
             The Reckoning
           </a>
+
+                    <a
+            className="flex justify-center w-50 h-24 md:w-70 md:h-30 items-center rounded-sm border border-white/20 bg-[#2e3a2f] px-6 font-[cursive] text-lg md:text-2xl font-bold tracking-wider text-white transition-colors duration-300 hover:border-[#e0b45b]"
+            href=""
+          >
+            Find My Housse
+          </a>
+
+                    <a
+            className="flex justify-center w-50 h-24 md:w-70 md:h-30 items-center rounded-sm border border-white/20 bg-[#3a2e2e] px-6 font-[cursive] text-lg md:text-2xl font-bold tracking-wider text-white transition-colors duration-300 hover:border-[#e0b45b]"
+            href=""
+          >
+            History
+          </a>
         </div>
       </div>
     </section>
