5478551c08cd394fe52f0762280b67293e2841dc
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-03-18 12:24:46 +0000

    Added quiz data structure - has placeholder text for now

---
+++ src/app/find-my-house/components/quizData.ts
@@ -0,0 +1,82 @@
+import { Question } from './quizTypes';
+
+
+//stores all of the questions and answer options
+export const questions: Question[] = [
+    {
+        id: 1,
+        question: 'Question 1 here',
+        options: [
+            { label: 'A', text: 'Answer A' },
+            { label: 'B', text: 'Answer B' },
+            { label: 'C', text: 'Answer C' },
+            { label: 'D', text: 'Answer D' },
+        ],
+    },
+
+    {
+        id: 2,
+        question: 'Question 2 here',
+        options: [
+            { label: 'A', text: 'Answer A' },
+            { label: 'B', text: 'Answer B' },
+            { label: 'C', text: 'Answer C' },
+            { label: 'D', text: 'Answer D' },
+        ],
+    },
+
+    {
+        id: 3,
+        question: 'Question 3 here',
+        options: [
+            { label: 'A', text: 'Answer A' },
+            { label: 'B', text: 'Answer B' },
+            { label: 'C', text: 'Answer C' },
+            { label: 'D', text: 'Answer D' },
+        ],
+    },
+
+    {
+        id: 4,
+        question: 'Question 4 here',
+        options: [
+            { label: 'A', text: 'Answer A' },
+            { label: 'B', text: 'Answer B' },
+            { label: 'C', text: 'Answer C' },
+            { label: 'D', text: 'Answer D' },
+        ],
+    },
+
+    {
+        id: 5,
+        question: 'Question 5 here',
+        options: [
+            { label: 'A', text: 'Answer A' },
+            { label: 'B', text: 'Answer B' },
+            { label: 'C', text: 'Answer C' },
+            { label: 'D', text: 'Answer D' },
+        ],
+    },
+
+    {
+        id: 6,
+        question: 'Question 6 here',
+        options: [
+            { label: 'A', text: 'Answer A' },
+            { label: 'B', text: 'Answer B' },
+            { label: 'C', text: 'Answer C' },
+            { label: 'D', text: 'Answer D' },
+        ],
+    },
+
+    {
+        id: 7,
+        question: 'Question 7 here',
+        options: [
+            { label: 'A', text: 'Answer A' },
+            { label: 'B', text: 'Answer B' },
+            { label: 'C', text: 'Answer C' },
+            { label: 'D', text: 'Answer D' },
+        ],
+    },
+];
