cf8590a7975e8dc5ab8b802ee0efaf70d9fd0bac
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-03-21 12:40:52 +0000

    Added AnswerOption type of quiz answers + Question type to structure the quiz data

--- src/app/xi/components/PDFViewer.tsx
+++ src/app/xi/components/PDFViewer.tsx
@@ -1,2 +1,16 @@
 //PDF rendering engine -- uses react-pdf
 //Displays the actual document
+
+
+export type AnswerOption = {
+    id: string;
+    text: string;   //answer shown to user
+};
+
+//Represents a question in the quiz
+export type Question = {
+    id: number;                 //Question number
+    question: string;           //Question text
+    options: AnswerOption[];    //Array of 4 possible answers 
+    correctAnswerID: string;    //The correct option id
+};
