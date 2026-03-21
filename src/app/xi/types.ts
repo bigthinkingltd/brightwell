ad7822f802100aed932f1ae4699e81a5ba63c34d
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-03-21 12:46:53 +0000

    Like an absolute moron i typed the code into the wrong file. Copied and paste into correct one now

--- src/app/xi/types.ts
+++ src/app/xi/types.ts
@@ -1,1 +1,30 @@
 //Structure of the data - what a question and answer looks like. 
+
+
+export type AnswerOption = {
+    id: string;
+    text: string;   //answer shown to user
+};
+
+//Represents a question in the quiz
+export type Question = {
+    id: number;                 //Question number
+    question: string;           //Question text
+    options: AnswerOption[];    //Array of 4 possible answers 
+    correctAnswerID: string;    //The correct option id
+};
+
+//Represents the different screens in the XI flow
+export type ScreenState = 
+    | 'intro'
+    |'question'
+    |'granted'
+    |'denied';
+
+
+//Represents PDF document in the archive
+export type PdfDocument = {
+    id: string;
+    title: string;
+    fileUrl: string; 
+};
