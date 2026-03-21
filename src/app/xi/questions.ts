dade844ddeb3d140f10b49e50dfafee76ca7a5c9
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-03-21 12:53:29 +0000

    Added question structure for 1 of the questions

--- src/app/xi/questions.ts
+++ src/app/xi/questions.ts
@@ -1,2 +1,19 @@
 //This is for the content of the quiz. 
 //It holds the question text + 4 possible answers for each question
+
+import { Question } from './types';
+
+export const questions: Question[] = [
+    {
+        id: 1,
+        question: 'The question goes here dec',
+        options: [
+            { id: 'a', text: 'Option for answer' },
+            { id: 'b', text: 'Option for answer' },
+            { id: 'c', text: 'Option for answer' },
+            { id: 'd', text: 'Option for answer' },
+        ],
+        correctAnswerId: 'd',
+    },
+
+];
