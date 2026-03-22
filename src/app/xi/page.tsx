b6d39c946f244eef6aeab6f331ff8de1fc292ca8
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-03-22 09:03:37 +0000

    use client and page imports

--- src/app/xi/page.tsx
+++ src/app/xi/page.tsx
@@ -2,3 +2,11 @@
 //Decides what screen is shown -  intro, questions, denied, granted
 //Tracks current questions and answers
 //Handles the validation for pass/fail
+
+
+'use client';
+
+import { useState } from 'react';
+import { questions } from './questions';
+import { ScreenState } from './types';
+
