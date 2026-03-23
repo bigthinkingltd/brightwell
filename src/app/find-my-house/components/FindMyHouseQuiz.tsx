9073241894f9d386f004df9a9b83be03421b5259
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-03-23 16:26:10 +0000

    Added imports that will be used on the page

---
+++ src/app/find-my-house/components/FindMyHouseQuiz.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { useMemo, useState } from "react";
+import QuizQuestionCard from "./QuizQuestionCard";
+import QuizResults from "./QuizResults";
+import { questions } from "./quizData";
+import { houseResults } from "./houseResults";
+import { AnswerOptions } from "./quizTypes";
+
+
