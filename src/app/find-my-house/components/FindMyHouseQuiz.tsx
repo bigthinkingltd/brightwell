275bafee6cfd733d826ce7132f916b33dc7a810b
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-03-23 16:40:14 +0000

    Added code to store user answers, track wehther quiz has been submitted and check for whether questions have been answered. Will continue work shortly

--- src/app/find-my-house/components/FindMyHouseQuiz.tsx
+++ src/app/find-my-house/components/FindMyHouseQuiz.tsx
@@ -8,3 +8,29 @@ import { houseResults } from "./houseResults";
 import { AnswerOptions } from "./quizTypes";
 
 
+/*Stores the users answers
+Key= question ID
+value = selected answer, or null if not answered */
+export default function FindMyHousequiz() {
+    const [answers, setAnswers] = useState<Record<number, AnswerOption | null>>({
+        1: null,
+        2: null,
+        3: null,
+        4: null,
+        5: null,
+        6: null,
+        7: null,
+    });
+
+    /*tracks whether user has submitted the quiz, if true it shows results, if not it shows the questions */
+    const [showResult, setShowResult] = useState(false);
+
+    /* Checks if all questiosn have been answered, used to prevent submission before completion */
+    const allQuestionsAnswered = useMemo (() => {
+        return questions.every((q) => answers [q.id] !== null);
+    }, [answers]);
+
+
+
+
+}
