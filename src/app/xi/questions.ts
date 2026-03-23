c4439718226247a59ba711b5f2761cb925dc6aa3
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-03-23 10:14:35 +0000

    Added the question text into the blocks for them

--- src/app/xi/questions.ts
+++ src/app/xi/questions.ts
@@ -6,36 +6,36 @@ import { Question } from './types';
 export const questions: Question[] = [
     {
         id: 1,
-        question: 'The question goes here dec',
+        question: "What is the name of Arlo's Grandfather?",
         options: [
-            { id: 'a', text: 'Option for answer' },
-            { id: 'b', text: 'Option for answer' },
-            { id: 'c', text: 'Option for answer' },
-            { id: 'd', text: 'Option for answer' },
+            { id: 'a', text: 'Halric' },
+            { id: 'b', text: 'Thalen' },
+            { id: 'c', text: 'Ren' },
+            { id: 'd', text: 'Atticus' },
         ],
         correctAnswerId: 'd',
     },
 
         {
         id: 2,
-        question: 'The question goes here dec',
+        question: 'What colour smoke accompanies a Portal?',
         options: [
-            { id: 'a', text: 'Option for answer' },
-            { id: 'b', text: 'Option for answer' },
-            { id: 'c', text: 'Option for answer' },
-            { id: 'd', text: 'Option for answer' },
+            { id: 'a', text: 'Purple' },
+            { id: 'b', text: 'Green' },
+            { id: 'c', text: 'Blue' },
+            { id: 'd', text: 'Red' },
         ],
         correctAnswerId: 'a',
     },
 
         {
         id: 3,
-        question: 'The question goes here dec',
+        question: 'In which House does Kit Featheringale belong?',
         options: [
-            { id: 'a', text: 'Option for answer' },
-            { id: 'b', text: 'Option for answer' },
-            { id: 'c', text: 'Option for answer' },
-            { id: 'd', text: 'Option for answer' },
+            { id: 'a', text: 'Wonder' },
+            { id: 'b', text: 'Watch' },
+            { id: 'c', text: 'Wisdom' },
+            { id: 'd', text: 'Wild' },
         ],
         correctAnswerId: 'b',
     },
