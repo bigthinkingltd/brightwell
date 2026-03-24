5f1a89ce9b3e54bbe847db54757fdb6a979179f7
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-03-24 16:19:23 +0000

    Added in the content text from the customer for the quiz result page

--- src/app/find-my-house/components/houseResults.ts
+++ src/app/find-my-house/components/houseResults.ts
@@ -11,25 +11,25 @@ After the quiz is complete:
 export const houseResults: Record<AnswerOption, HouseResults> = {
     A: {
     houseName: 'House of Wisdom', //house name
-    motto: 'ENTER HERE', //The motto for the house
-    description: 'ENTER HERE', //The longer explanation of the house... as i type thi its pointless having a motto AND description
+    motto: '', //The motto for the house
+    description: 'You’re thoughtful, curious, and always searching for truth. You carry the Staff of Further Enquiry and never stop asking why.', //The longer explanation of the house... as i type thi its pointless having a motto AND description
     },
 
     B: {
-        houseName: 'ENTER HERE',
-        motto: 'ENTER HERE',
-        description: 'ENTER HERE',
+        houseName: 'House of Watch',
+        motto: '',
+        description: 'You’re brave, loyal, and full of conviction. You protect others and stand tall when things get tough — your heart burns bright as the Ember.',
     },
 
     C: {
-        houseName: 'ENTER HERE', 
-        motto: 'ENTER HERE', 
-        description: 'ENTER HERE', 
+        houseName: 'House of Wild', 
+        motto: '', 
+        description: 'You’re kind, instinctive, and deeply connected to the world around you. You understand that magic is everywhere, especially in nature’s quiet places.', 
     },
 
     D: {
-        houseName: 'ENTER HERE', 
-        motto: 'ENTER HERE',
-        description: 'ENTER HERE', 
+        houseName: 'House of Wonder', 
+        motto: '',
+        description: 'You’re imaginative, daring, and delightfully unpredictable. You see possibility in everything — and everything in possibility.', 
     },
 };
