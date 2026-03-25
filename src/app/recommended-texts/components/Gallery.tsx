c5730eba2a9f912525b610dedb6599e32cdaa5b3
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-03-25 20:33:51 +0000

    Added 3 images to function on gallery page

--- src/app/recommended-texts/components/Gallery.tsx
+++ src/app/recommended-texts/components/Gallery.tsx
@@ -1,1 +1,22 @@
 //This is where the 3 image covers will be displayed- it will handle the layout of the images and the clicks when the user presses
+
+
+export default function Gallery() {
+  const texts = [
+    {
+      id: 'brightspell',
+      image: '/recommended-texts/image-covers/brightspell.png',
+      width: 'flex-1',
+    },
+    {
+      id: 'scorch',
+      image: '/recommended-texts/image-covers/scorch.png',
+      width: 'flex-[0.7]',
+    },
+    {
+      id: 'questerling',
+      image: '/recommended-texts/image-covers/questerling.png',
+      width: 'flex-1',
+    },
+  ]
+}
