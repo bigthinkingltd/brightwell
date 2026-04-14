3d20884f4a30c5c6dd28ccaf832790d088760f3d
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-04-14 21:27:31 +0100

    Added imports

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,2 +1,7 @@
 //Server only
 //Handles communication with S3 and generates presigned URL, allowing browser temorary access to PDFs
+
+import 'server-only';
+
+import { S3Client, GetOjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
