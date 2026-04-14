8c6d9c37023ab7bc58c8cd86cec738273cba93bb
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-04-14 20:51:31 +0100

    Added file name

--- src/lib/pdfs.ts
+++ src/lib/pdfs.ts
@@ -8,6 +8,7 @@ export type PdfDisposition = 'inline' | 'download';
 //Waht the PDF object looks like
 export type PdfDefinition = {
   key: string;                  //where it lives in S3
+  fileName: string;
   disposition: PdfDisposition;  // how it behaves
   access: PdfAccess;            //who can access 
 };
@@ -16,21 +17,25 @@ export type PdfDefinition = {
 export const pdfRegistry: Record<string, PdfDefinition> = {
   'recommended-text-1': {
     key: 'pdfs/recommended/recommended-text-1.pdf',
+    fileName: 'recommended-text-1.pdf',
     disposition: 'download', //downloadable
     access: 'public',
   },
   'recommended-text-2': {
     key: 'pdfs/recommended/recommended-text-2.pdf',
+    fileName: 'recommended-text-2.pdf'
     disposition: 'download', //downloadable
     access: 'public',
   },
   'xi-pdf-1': {
     key: 'pdfs/xi/xi-pdf-1.pdf',
+    fileName: 'xi-pdf-1.pdf',
     disposition: 'inline', //viewable
     access: 'quiz',
   },
   'xi-pdf-2': {
     key: 'pdfs/xi/xi-pdf-2.pdf',
+    fileName: 'xi-pdf-2.pdf',
     disposition: 'inline', //viewable
     access: 'quiz',
   },
