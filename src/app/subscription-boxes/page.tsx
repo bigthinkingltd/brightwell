78a72b77cfa6ecdf070414dca983a2012b9d483a
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-04-17 08:58:55 +0100

    Added header image to public folder + got code to render image on page, however its not formated and doesnt have background removed yet

--- src/app/subscription-boxes/page.tsx
+++ src/app/subscription-boxes/page.tsx
@@ -3,3 +3,25 @@
 import Image from "next/image";
 import Link from "next/link";
 
+export default function SubscriptionBoxesPage() {
+  return (
+    <main className="min-h-screen bg-[#efe6d6] px-6 py-16 text-[#2f2418]">
+      <div className="min-h-screen bg-[#efe6d6] px-6 py-16 text-[#2f2418]">
+
+        {/*the header image, will sit above everything. */}
+        <div className="mb-10 w-full max-w-3xl">
+          <Image
+            src="/subscription-boxes.png"
+            alt="Brightwell Academy Header"
+            width={1200}
+            height={300}
+            className="w-full h-auto object-contain"
+            priority //want it to load first. 
+            />
+        </div>
+      </div>
+
+
+    </main>
+  )
+}
