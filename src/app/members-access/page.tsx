5b7cf7cf5cd9e2e8786cc216d1b366410bd132e9
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-04-19 14:26:32 +0100

    use client + imports

--- src/app/members-access/page.tsx
+++ src/app/members-access/page.tsx
@@ -1,2 +1,6 @@
 //Will be the entry point for the members area
 //Users will come to this page and enterr a universal access code (Sent via post or email)
+
+'use client';
+
+import { FormEvent, useState } from 'react';
