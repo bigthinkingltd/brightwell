cf75e2e3718f08f78cfc59a6473a8ed9a5a4191e
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-04-19 14:40:00 +0100

    Added imports

--- src/app/api/memebrs-access/route.ts
+++ src/app/api/memebrs-access/route.ts
@@ -1,2 +1,4 @@
 //API route will handle the access code submisssion from the access page
 //Will compare the submitted code against the server-side env variable
+
+import { NextResponse } from 'next/server';
