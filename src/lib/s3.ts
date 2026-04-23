0b328a329fb395b4afed265aff07ce9785b8d388
Declan W <148910875+DeclanW1@users.noreply.github.com>
2026-04-23 13:36:49 +0100

    added required AWS config details and errors for missing env details

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -5,3 +5,28 @@ import 'server-only';
 
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import type { PdfDisposition } from './pdfs';
+
+//will read required AWS config from environment variables 
+const region = process.env.AWS_REGION;
+const bucketName = process.env.AWS_S3_Bucket_Name;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+//failures for missing environment variables 
+//config problems arrise as soon as server starts
+if (!region) {
+    throw new Error('Missing environment variable: AWS_REGION');
+}
+
+if (!bucketName) {
+    throw new Error('Missing environment variable: AWS_S3_BUCKET_NAME');
+}
+
+if (!accessKeyId) {
+    throw new Error('Missing environment variable: AWS_ACCESS_KEY_ID');
+}
+
+if (!secretAccessKey) {
+    throw new Error('Missing enviroment variable: AWS_SECRET_ACCESS_KEY');
+}
